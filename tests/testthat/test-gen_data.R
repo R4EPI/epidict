2147de2a77d38526b64ad6c411f822b8b8bd2ac9
Zhian N. Kamvar <zkamvar@gmail.com>
2020-01-17 14:14:31 +0000

    remove gen_polygon

--- tests/testthat/test-gen_data.R
+++ tests/testthat/test-gen_data.R
@@ -100,9 +100,3 @@ test_that("survey data can be generated", {
 })
 
 
-test_that("polygons can be generated", {
-
-  poly <- suppressWarnings(gen_polygon(LETTERS[1:4]))
-  expect_equal(dim(poly), c(4, 2))
-
-})
