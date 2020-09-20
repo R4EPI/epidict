2f0a23eace541ccfabe5c568ef4b73bb13a667d5
aspina7 <alexfspina@gmail.com>
2020-09-20 14:20:26 +0100

    working dictionary for mort and vacc

--- R/msf_dict_survey.R
+++ R/msf_dict_survey.R
@@ -7,8 +7,7 @@
 #' @export
 #' @rdname msf_dict
 msf_dict_survey <- function(disease, name = "MSF-survey-dict.xlsx",
-                            tibble = TRUE,
-                            compact = FALSE) {
+                            tibble = TRUE, compact = TRUE, long = TRUE) {
 
   disease <- get_dictionary(disease)$survey
 
@@ -28,73 +27,77 @@ msf_dict_survey <- function(disease, name = "MSF-survey-dict.xlsx",
   colnames(dat_dict) <- tidy_labels(colnames(dat_dict))
   colnames(dat_opts) <- tidy_labels(colnames(dat_opts))
 
+  # clean future var names
+  dat_dict$name <- tidy_labels(dat_dict$name)
+
+  # pre-pend "choices" to options dataset (otherwise duplicated column names on merge)
+  colnames(dat_opts) <- sprintf("choices_%s", colnames(dat_opts))
+
   # drop rows with grouping variables for repeats (not necessary for generating data)
   dat_dict <- dat_dict[!dat_dict$type %in% c("begin_group", "end_group", "end_repeat"), ]
 
 
-  dat_dict <- dplyr::rename_at(dat_dict,
-    .vars = dplyr::vars(dplyr::starts_with("choice_")),
-    .funs = ~ gsub("choice", "option", .)
-  )
-
-  # minor tidying, e.g.: create "CodeX" assignments
+  # minor tidying, make the type column useful as as unique ID for options
   dat_dict$type <- gsub(
     pattern = "select_one |select_multiple ",
     replacement = "",
     x = dat_dict$type
   )
 
-  # transform dat_dict to wide format (like outbreak dictionary)
-  dat_dict <- dplyr::select(
-    dat_dict,
-    !!quote(level),
-    !!quote(column_name),
-    !!quote(description),
-    !!quote(type),
-    dplyr::starts_with("option_")
-  )
+  # create counts for variable order (not entirely sure this is needed?)
+  # dat_dict <- dplyr::mutate(dat_dict, option_order_in_set = seq(dplyr::n()))
 
-  dat_dict <- dplyr::group_by(dat_dict, !!quote(column_name))
+  ## recode type to fit to DHIS2 (I dont think this is necessary?)
+  # dat_dict$type <- dplyr::case_when(
+  #   dat_dict$type == "Integer" ~ "INTEGER_POSITIVE",
+  #   dat_dict$type == "Binary" ~ "TEXT",
+  #   dat_dict$type == "ChoiceMulti" ~ "MULTI",
+  #   dat_dict$type == "Text" ~ "LONG_TEXT",
+  #   dat_dict$type == "Geo" ~ "LONG_TEXT",
+  #   dat_dict$type == "Date" ~ "DATE",
+  #   dat_dict$type == "Choice" ~ "TEXT",
+  #   dat_dict$type == "Number" ~ "INTEGER_POSITIVE"
+  # )
 
-  dat_dict <- dplyr::mutate(dat_dict, option_order_in_set = seq(dplyr::n()))
 
-  if (compact) {
-    if (utils::packageVersion("tidyr") > "0.8.99") {
-      dat_dict <- tidyr::nest(dat_dict, options = dplyr::starts_with("option_"))
-    } else {
-      squished <- tidyr::nest(dat_dict, dplyr::starts_with("option_"), .key = "options")
-      dat_dict <- dplyr::select(dat_dict, -dplyr::starts_with("option_"))
-      dat_dict <- dplyr::distinct(dat_dict)
-      dat_dict <- dplyr::left_join(dat_dict, squished, by = "column_name")
+
+  if (long) {
+    outtie <- dplyr::left_join(dat_dict, dat_opts,
+                               by = c("type" = "choices_list_name")
+    )
+
+    outtie <- if (tibble) tibble::as_tibble(outtie) else outtie
+
+    # Return second option: a list with data dictionary and value options seperate
+  } else {
+    if (tibble) {
+      dat_dict <- tibble::as_tibble(dat_dict)
+      dat_opts <- tibble::as_tibble(dat_opts)
     }
-  }
-  dat_dict <- dplyr::ungroup(dat_dict)
-
-
-  dat_dict$type <- dplyr::case_when(
-    dat_dict$type == "Integer" ~ "INTEGER_POSITIVE",
-    dat_dict$type == "Binary" ~ "TEXT",
-    dat_dict$type == "ChoiceMulti" ~ "MULTI",
-    dat_dict$type == "Text" ~ "LONG_TEXT",
-    dat_dict$type == "Geo" ~ "LONG_TEXT",
-    dat_dict$type == "Date" ~ "DATE",
-    dat_dict$type == "Choice" ~ "TEXT",
-    dat_dict$type == "Number" ~ "INTEGER_POSITIVE"
-  )
-
-  dat_dict <- dplyr::rename(dat_dict, "data_element_valuetype" = "type")
-
-  # clean future var names
-  # excel names (data element shortname)
-  # csv names (data_element_name)
-  dat_dict$column_name <- tidy_labels(dat_dict$column_name)
-
-  dat_dict <- data.frame(dat_dict, stringsAsFactors = FALSE)
-
-  # return a tibble
-  if (compact || tibble) {
-    dat_dict <- tibble::as_tibble(dat_dict)
+    outtie <- list(
+      dictionary = dat_dict,
+      options    = dat_opts
+    )
   }
 
-  dat_dict
+  # produce clean compact data dictionary for use in gen_data
+  if (long && compact == TRUE) {
+    squished <- dplyr::group_by(outtie, !!quote(name))
+
+    if (utils::packageVersion("tidyr") > "0.8.99") {
+      squished <- tidyr::nest(squished, options = dplyr::starts_with("choices_"))
+    } else {
+      ## not sure this is going to be the case anymore?? can probably delete
+      squished <- tidyr::nest(squished, dplyr::starts_with("choices_"), .key = "options")
+      outtie <- dplyr::select(outtie, -dplyr::starts_with("choices_"))
+      outtie <- dplyr::distinct(outtie)
+      squished <- dplyr::left_join(outtie, squished, by = c("type" = "choices_list_name"))
+    }
+    return(dplyr::ungroup(squished))
+  }
+
+
+  # return dictionary dataset
+  outtie
+
 }
