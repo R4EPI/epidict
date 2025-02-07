4516cdf6f2279f9f28717614101c379515b4e70e
Patrick Keating <Patrick.Keating@london.msf.org>
2025-02-07 09:57:14 +0000

    As before, putting EBS to ebs

--- R/msf_dict_survey.R
+++ R/msf_dict_survey.R
@@ -22,7 +22,7 @@ msf_dict_survey <- function(disease, name = "MSF-survey-dict.xlsx",
     disease <- get_dictionary(disease, org = "MSF")$survey
 
     if (length(disease) == 0) {
-      stop("disease must be one of 'Mortality', 'Nutrition', 'Vaccination_long', 'Vaccination_short', 'EBS'", call. = FALSE)
+      stop("disease must be one of 'Mortality', 'Nutrition', 'Vaccination_long', 'Vaccination_short', 'ebs'", call. = FALSE)
     }
     # get excel file path (need to specify the file name)
     path <- system.file("extdata", name, package = "epidict")
